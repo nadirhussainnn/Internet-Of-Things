e81df0d621bfbc70886525b86bbe7fe0ce62c861
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-01-16 00:03:12 +0500

    Updated

--- ThingSpeak/NodeJS+ThingSpeak/index.js
+++ ThingSpeak/NodeJS+ThingSpeak/index.js
@@ -8,20 +8,20 @@ app.use(cors());
 app.use(express.json());
 
 const PORT=5000;
-const KEY=`UHXSCM81HU5K5WKK`;         //provide your ThingSpeak API Key here: 
-                                      //This is mine and will not work for you
+// const KEY=`UHXSCM81HU5K5WKK`;         //provide your ThingSpeak API Key here: 
+//                                       //This is mine and will not work for you
 
 
-app.get("/api/sendToThingWorks",(req, resp)=>{
+app.post("/api/sendToThingWorks",(req, resp)=>{
 
   // This is public API from my ThingSpeak Account
   //field1: temperature field
   //field2: Humidity field
+  const temperature=req.body.TEMPERATURE;
+  console.log(temperature);
 
-  got.get(`https://api.thingspeak.com/update?api_key=${KEY}&field1=${78}`)
+  got.get(`https://api.thingspeak.com/update?api_key=UHXSCM81HU9K9WKK&field1=${temperature}`)
   .then(res => {
-
-    console.log(res)
 
   })
   .catch(err => {
@@ -35,3 +35,9 @@ app.get("/api/sendToThingWorks",(req, resp)=>{
 app.listen(PORT, () => {
     console.log(`Server has started at port ${PORT}`);
 });
+
+
+//Using NodeMCU with ThingsSpeak
+
+//Download ThingsSpeak zip library then add into Arduino as Sketch->Library->Add zip library and write code
+//Connect NodeMCU with Arduino IDE so include link: Install ESP8266 in arduino tools
